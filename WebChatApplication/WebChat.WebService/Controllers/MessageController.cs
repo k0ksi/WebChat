56e776c1c17cc9a407547f356b0a63ec69877a2f
HristoVutov <vutov23@gmail.com>
2015-09-01 15:37:41 +0300

    Adding Message Methods in the Controller
    .

--- WebChatApplication/WebChat.WebService/Controllers/MessageController.cs
+++ WebChatApplication/WebChat.WebService/Controllers/MessageController.cs
@@ -13,7 +13,7 @@ namespace WebChat.WebService.Controllers
     [Authorize]
     public class MessageController : BaseApiController
     {
-        //POST api/messages/posts
+        //POST api/messages
         [HttpPost]
         public IHttpActionResult SendMessage(
             [FromBody]MessageBindingModels model)
@@ -43,8 +43,62 @@ namespace WebChat.WebService.Controllers
 
             var message = new Message()
             {
+                SenderId = sender.Id,
+                ReceiverId = reciever.Id,
+                MessageDateTime = DateTime.Now,
+                Content = model.Content
+
+            };
+
+
+            this.Data.Messages.Add(message);
+            this.Data.SaveChanges();
+
+            var data = this.Data.Messages
+                .Where(p => p.Id == message.Id)
+                .Select(MessageViewModels.Create)
+                .FirstOrDefault();
+
+            return this.Ok(data);
+        }
+
+        //GET api/messages
+        [HttpGet]
+        [AllowAnonymous]
+        public IHttpActionResult GetAllMessages()
+        {            
+            var data = this.Data.Messages
+                .OrderBy(m => m.MessageDateTime)
+                .Select(MessageViewModels.Create);
+
+            return this.Ok(data);
+        }
+
+        //POST api/messages/{roomID}
+        [HttpPost]
+        public IHttpActionResult SendMessage(
+            [FromBody]MessageBindingModels model,int id)
+        {
+            if (model == null)
+            {
+                return this.BadRequest("Model cannot be null (no data in request)");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            
+
+            string senderName = this.User.Identity.Name;
+
+            var sender = this.Data.Users.FirstOrDefault(u => u.UserName == senderName);
+
+            var message = new Message()
+            {
                  SenderId = sender.Id,
-                 ReceiverId = reciever.Id,
+                 ChatRoomId = id,
                  MessageDateTime = DateTime.Now,
                  Content = model.Content
 
@@ -62,12 +116,13 @@ namespace WebChat.WebService.Controllers
             return this.Ok(data);
         }
 
-        //GET api/messages/posts
+        //GET api/messages/{roomID}
         [HttpGet]
         [AllowAnonymous]
-        public IHttpActionResult GetAllMessages()
-        {            
+        public IHttpActionResult GetAllMessages(int roomId)
+        {
             var data = this.Data.Messages
+                .Where(m => m.ChatRoomId == roomId)
                 .OrderBy(m => m.MessageDateTime)
                 .Select(MessageViewModels.Create);
 
